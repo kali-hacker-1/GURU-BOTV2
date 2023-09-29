7fe50f96612e0b2842f9ee21f63b44ffe96f45b9
kali-hacker-1 <81081579+kali-hacker-1@users.noreply.github.com>
2023-09-29 17:00:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['916300793116', 'HASHIRAMA', true],
-  ['918712063999', 'WORKER', true], 
+  ['919247890502', 'MV', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "919247890502" //put your bot number here
 
-global.mods = ['916300793116','918712063999'] 
-global.prems = ['916300793116', '918712063999', '919398758484']
-global.allowed = ['916300793116', '918712063999', '919182063909']
+global.mods = ['916300793116','919247890502'] 
+global.prems = ['916300793116', '919247890502', '919398758484']
+global.allowed = ['916300793116', '919247890502', '919182063909']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
